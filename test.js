e499bc3ed229e36382e488de2a24e25a4ae8806f
Zoltan Kochan <z@kochan.io>
2017-12-25 13:01:59 +0200

    test: test loosely

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ test('getVersionSelectorType()', t => {
   t.throws(() => getVersionSelectorType(), /should be a string/)
   t.throws(() => getVersionSelectorType(1), /should be a string/)
   t.equal(getVersionSelectorType('1.0.0'), 'version')
+  t.equal(getVersionSelectorType('= 1.0.0', true), 'version')
   t.equal(getVersionSelectorType('^1.0.0'), 'range')
   t.equal(getVersionSelectorType('latest'), 'tag')
   t.equal(getVersionSelectorType('github:zkochan/foo'), null)
